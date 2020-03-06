d9bf68aa8f5dc11b68772199f3e494032df086b0
admin <admin@1A-1>
2020-03-06 14:46:56 +0530

    d

--- dummy/src/main/java/com/example/demo/DummyApplication.java
+++ dummy/src/main/java/com/example/demo/DummyApplication.java
@@ -1,15 +1,20 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.
+SpringBootApplication;
 
 @SpringBootApplication
-public class DummyApplication {
+public class DummyApplication 
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DummyApplication.class, args);
-		
+	
+	
 		System.out.println("gopallllllllllll");
+		System.out.println("Hello   ");
+	
 	}
 
 }
