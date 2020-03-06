efdb0433af070d8ebc8167e80339db17c3f70cd5
Gopaltiramsetti <48171892+Gopaltiramsetti@users.noreply.github.com>
2020-03-06 11:06:35 +0530

    Update Helloworld.java

--- sample/src/main/java/com/example/demo/Helloworld.java
+++ sample/src/main/java/com/example/demo/Helloworld.java
@@ -3,7 +3,7 @@ package com.example.demo;
 public class Helloworld {
 	private void psvm() {
 		// TODO Auto-generated method stub
-System.out.println("hello");
+System.out.println("hello pradeep vadde");
 	}
 
 }
