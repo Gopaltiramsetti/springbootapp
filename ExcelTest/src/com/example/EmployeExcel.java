c6a8f1d422c30390b75b313c357558936175abb4
admin <admin@1A-1>
2020-03-19 10:12:12 +0530

    Excel

---
+++ ExcelTest/src/com/example/EmployeExcel.java
@@ -0,0 +1,100 @@
+package com.example;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class EmployeExcel {
+	public static void main(String[] args) {
+        String jdbcURL = "jdbc:mysql://localhost:3306/emptest";
+        String username = "root";
+        String password = "root";
+ 
+        String filepath = "D:\\employee.xlsx";
+ 
+        int batchSize = 20;
+ 
+        Connection connection = null;
+ 
+        try {
+             
+            FileInputStream inputStream = new FileInputStream(filepath);
+ 
+            Workbook workbook = new XSSFWorkbook(inputStream);
+ 
+            org.apache.poi.ss.usermodel.Sheet firstSheet = workbook.getSheetAt(0);
+            Iterator<Row> rowIterator = firstSheet.iterator();
+ 
+            connection = DriverManager.getConnection(jdbcURL, username, password);
+            connection.setAutoCommit(false);
+  
+            String sql = "INSERT INTO emptest (name, gender, location) VALUES (?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(sql);    
+             
+            int count = 0;
+             
+            rowIterator.next(); // skip the header row
+             
+            while (rowIterator.hasNext()) {
+                Row nextRow = rowIterator.next();
+                Iterator<Cell> cellIterator = nextRow.cellIterator();
+ 
+                while (cellIterator.hasNext()) {
+                    Cell nextCell = cellIterator.next();
+ 
+                    int columnIndex = nextCell.getColumnIndex();
+ 
+                    switch (columnIndex) {
+                    case 0:
+                        String name = nextCell.getStringCellValue();
+                        statement.setString(1, name);
+                        break;
+                    case 1:
+                    	String gender = nextCell.getStringCellValue();
+                        statement.setString(2, gender);
+                        break;
+                    case 2:
+                    	String location = nextCell.getStringCellValue();
+                        statement.setString(3, location);
+                    }
+                    count++;
+ 
+                }
+                 
+                statement.addBatch();
+                 
+                if (count % batchSize == 0) {
+                    statement.executeBatch();
+                }              
+ 
+            }
+ 
+            workbook.close();
+             
+            // execute the remaining queries
+            statement.executeBatch();
+  
+            connection.commit();
+            connection.close();
+             
+           
+             
+        } catch (IOException ex1) {
+            System.out.println("Error reading file");
+            ex1.printStackTrace();
+        } catch (SQLException ex2) {
+            System.out.println("Database error");
+            ex2.printStackTrace();
+        }
+ 
+    }
+}
